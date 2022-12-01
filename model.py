f0d7656e64498889189b52f33cee896328df94f7
PrrsnCrVS4sr <96426776+PrrsnCrVS4sr@users.noreply.github.com>
2022-12-02 00:59:45 +0530

    implemented model and a bit of trainer code

--- model.py
+++ model.py
@@ -8,13 +8,13 @@ import os
 class Linear_QNet(nn.Module):
     def __init__(self,input_size,hidden_size,output_size):
         super().__init__()
-        pass
+        self.linear1 = nn.Linear(in_features=input_size,out_features=hidden_size)
+        self.linear2 = nn.Linear(in_features=hidden_size,out_features=output_size)
     def forward(self,x):
-        pass
+        return self.linear2((F.relu(self.linear1(x))))
 
     def save(self,file_name ='model.pth'):
         model_folder_path = './model'
-
         if not os.path.exists(model_folder_path):
             os.makedirs(model_folder_path)
         file_name = os.path.join(model_folder_path,file_name)
@@ -24,6 +24,12 @@ class QTrainer:
     def __init__(self,model,lr,gamma):
         #set stuff like optimisers
         #use MSE loss
+        self.model = model
+        self.lr = lr
+        self.gamma = gamma
+        self.opt = optim.Adam(self.model.parameters(),lr)
+        self.loss = nn.MSELoss()
+        
         pass
 
     def train_step(self, state, action, reward, next_state, done):
