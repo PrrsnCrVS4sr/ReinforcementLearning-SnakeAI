e5e8922ce35f47d7ec03d875a8bedcbdc7c294e8
PrrsnCrVS4sr <96426776+PrrsnCrVS4sr@users.noreply.github.com>
2022-12-02 01:31:02 +0530

    implemented trainer

--- model.py
+++ model.py
@@ -33,9 +33,33 @@ class QTrainer:
         pass
 
     def train_step(self, state, action, reward, next_state, done):
+        state = torch.tensor(state,dtype=torch.float)
+        action = torch.tensor(action,dtype=torch.long)
+        reward = torch.tensor(reward,dtype=torch.float)
+        next_state = torch.tensor(next_state,dtype=torch.float)
+        done = torch.tensor(done,dtype=torch.float)
+
+        if len(state.shape) == 1:
+            state = torch.unsqueeze(state,0)
+            next_state = torch.unsqueeze(next_state,0)
+            action = torch.unsqueeze(action,0)
+            reward = torch.unsqueeze(reward,0)
+            done = (done,)
         #  each paramter can be single value or tupe to first convert to tensors
         # if single value, add a dimension using torch.unsqueeze
-
+        Q_predicted = self.model(state)
+        target = Q_predicted.clone()
+        for i in range(len(done)):
+            Q_new = reward[i]
+            if done[i] != True:
+                Q_new += (torch.max(self.model(next_state[i])).item() * self.gamma)
+            target[i][torch.argmax(action).item()] = Q_new
         # get predicted Q value with current state
         # 2. r + y* max(next_predicted Qvalue)
-        pass
+        self.opt.zero_grad()
+
+        loss = self.loss(target,Q_predicted)
+
+        loss.backward()
+
+        self.opt.step()
